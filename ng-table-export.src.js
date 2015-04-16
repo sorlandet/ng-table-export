39db5f052ced8d9e032ee91dc697d2cc786681a5
Evgeniy Medvedev <yevgeniy.medvedev@gmail.com>
2015-04-16 16:08:04 +0600

    buid package

--- ng-table-export.src.js
+++ ng-table-export.src.js
@@ -1,64 +1,64 @@
 angular.module('ngTableExport', [])
-.config(['$compileProvider', function($compileProvider) {
-    // allow data links
-    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|data):/);
-}])
-.directive('exportCsv', ['$parse', function ($parse) {
-    return {
-        restrict: 'A',
-        scope: false,
-        link: function(scope, element, attrs) {
-            var lineEnding = (attrs.exportCsvEnding && attrs.exportCsvEnding.indexOf('win')) ? '\r\n' : '\n';
-            var separator = attrs.exportCsvSeparator || ',';
-            var contentType = attrs.exportCsvContentType || 'text/csv';
+  .config(['$compileProvider', function($compileProvider) {
+      // allow data links
+      $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|data):/);
+  }])
+  .directive('exportCsv', ['$parse', function ($parse) {
+      return {
+          restrict: 'A',
+          scope: false,
+          link: function(scope, element, attrs) {
+              var lineEnding = (attrs.exportCsvEnding && attrs.exportCsvEnding.indexOf('win')) ? '\r\n' : '\n';
+              var separator = attrs.exportCsvSeparator || ',';
+              var contentType = attrs.exportCsvContentType || 'text/csv';
 
-            var data = '';
-            var csv = {
-                stringify: function(str) {
-                    return '"' +
+              var data = '';
+              var csv = {
+                  stringify: function(str) {
+                      return '"' +
                         str.replace(/^\s\s*/, '').replace(/\s*\s$/, '') // trim spaces
-                            .replace(/"/g,'""') + // replace quotes with double quotes
+                          .replace(/"/g,'""') + // replace quotes with double quotes
                         '"';
-                },
-                generate: function() {
-                    data = '';
-                    var rows = element.find('tr');
-                    angular.forEach(rows, function(row, i) {
-                        var tr = angular.element(row);
-                        if (tr.hasClass('ng-table-filters')) {
-                            return;
-                        }
-                        var tds = tr.find('th'), rowData = '', headerData = '', headers = true;
-                        if (tds.length == 0) {
-                            tds = tr.find('td');
-                            headers = false;
-                        }
-                        angular.forEach(tds, function(td, i) {
-                            var el = angular.element(td);
-                            var value = el.text();
-                            if (headers) {
-                                var title = el.data('title');
-                                if (typeof title != "undefined") {
-                                    value = title;
-                                }
-                                headerData += value;
-                            }
-                            rowData += csv.stringify(value) + separator;
-                        });
+                  },
+                  generate: function() {
+                      data = '';
+                      var rows = element.find('tr');
+                      angular.forEach(rows, function(row, i) {
+                          var tr = angular.element(row);
+                          if (tr.hasClass('ng-table-filters')) {
+                              return;
+                          }
+                          var tds = tr.find('th'), rowData = '', headerData = '', headers = true;
+                          if (tds.length == 0) {
+                              tds = tr.find('td');
+                              headers = false;
+                          }
+                          angular.forEach(tds, function(td, i) {
+                              var el = angular.element(td);
+                              var value = el.text();
+                              if (headers) {
+                                  var title = el.data('title');
+                                  if (typeof title != "undefined") {
+                                      value = title;
+                                  }
+                                  headerData += value;
+                              }
+                              rowData += csv.stringify(value) + separator;
+                          });
 
-                        if (headers && headerData.trim() === '') {
-                            return;
-                        }
-                        rowData = rowData.slice(0, rowData.length - 1); //remove last semicolon
+                          if (headers && headerData.trim() === '') {
+                              return;
+                          }
+                          rowData = rowData.slice(0, rowData.length - 1); //remove last semicolon
 
-                        data += rowData + lineEnding;
-                    });
-                },
-                link: function() {
-                    return 'data:' + contentType + ';charset=UTF-8,' + encodeURIComponent(data);
-                }
-            };
-            $parse(attrs.exportCsv).assign(scope.$parent, csv);
-        }
-    };
-}]);
+                          data += rowData + lineEnding;
+                      });
+                  },
+                  link: function() {
+                      return 'data:' + contentType + ';charset=UTF-8,' + encodeURIComponent(data);
+                  }
+              };
+              $parse(attrs.exportCsv).assign(scope.$parent, csv);
+          }
+      };
+  }]);
